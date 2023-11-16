b18c5034d412ce6190e43b69a8bab621f58776da
Zach Beck <zbeck@utah.gov>
2023-11-16 13:37:42 -0700

    Delete remap_layers.py

--- remap_layers.py
+++
@@ -1,9 +0,0 @@
-import arcpy
-import sweeper
-
-# lyrs = m.listLayers()
-# for l in lyrs:
-#     if l.supports('DATASOURCE'):
-#         source_gdb = os.path.split(l.dataSource)[0]
-#         if source_gdb == r'L:\caching\Data\SGID10_WGS.gdb':
-#             aprx.updateConnectionProperties(source_gdb, 'C:\Cache\MapData\SGID10_WGS.gdb')
